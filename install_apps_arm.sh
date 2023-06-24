bdc69806e7aaaee1edc72402da47023165e254d1
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2023-06-24 17:42:29 +0100

    Removed i686 on arm

--- install_apps_arm.sh
+++ install_apps_arm.sh
@@ -3,7 +3,5 @@ set -e
 
 wget --show-progress -O vivaldi.deb https://downloads.vivaldi.com/stable/vivaldi-stable_6.1.3035.100-1_arm64.deb
 
-sudo dpkg --add-architecture i686
-sudo apt update
-sudo apt install -y lib32stdc++6 ./vivaldi.deb gimp inkscape chromium filezilla
+sudo apt install -y ./vivaldi.deb gimp inkscape chromium filezilla
 
