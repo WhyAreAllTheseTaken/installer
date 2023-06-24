35ba5f05e49d6590e84b0e75ac88ebc6628cd50a
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2023-06-24 13:00:35 +0100

    Autoaccept installing cli packages

--- install_cli.sh
+++ install_cli.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 set -e
 
-sudo apt install zsh nnn htop neofetch cava network-manager
+sudo apt install -y zsh nnn htop neofetch cava network-manager
 
 ./install_nvim.sh
 ./copy_cli_dotfiles.sh
