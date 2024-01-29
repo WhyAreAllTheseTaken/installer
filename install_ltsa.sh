4567ca495af0fec190f2fc8f78796da9b32c9801
Tomaso2468 <48136416+Tomaso2468@users.noreply.github.com>
2024-01-29 09:56:54 +0000

    Add LTSA.

---
+++ install_ltsa.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+set -e
+
+# Install java
+sudo apt install -y unzip
+rm -fv ltsatool.zip
+rm -rfv ltsatool
+wget --show-progress -O ltsatool.zip https://www.doc.ic.ac.uk/~jnm/book/ltsa/ltsatool.zip
+unzip ltsatool.zip
+sudo rm -rfv /usr/local/share/ltsatool
+sudo cp -rv ./ltsatool /usr/local/share/
+
